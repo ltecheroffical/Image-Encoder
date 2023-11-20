4ff9d55bcca1aa03c2f8b2b813b5969446af1590
Lawrence <82725076+ltecheroffical@users.noreply.github.com>
2023-11-20 05:09:17 -0500

    Add Icon

--- addons/image_bytes/plugin.gd
+++ addons/image_bytes/plugin.gd
@@ -4,7 +4,8 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	add_custom_type("ImageBytes", "RefCounted", 
-		preload("res://addons/image_bytes/bytes_to_image.gd"), null)
+		preload("res://addons/image_bytes/bytes_to_image.gd"), 
+		preload("res://addons/image_bytes/icon.svg"))
 
 
 func _exit_tree() -> void:
